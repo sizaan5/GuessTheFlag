d838f0de27adea862c109293995261423b9837c0
Izaan Saleem <izaan.saleem@m3tech.com.pk>
2025-03-13 11:57:41 +0500

    Day 75/100 - Fixing Guess the Flag

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -20,6 +20,20 @@ struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes. Top stripe blue, middle stripe black, bottom stripe white.",
+        "France": "Flag with three vertical stripes. Left stripe blue, middle stripe white, right stripe red.",
+        "Germany": "Flag with three horizontal stripes. Top stripe black, middle stripe red, bottom stripe gold.",
+        "Ireland": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe orange.",
+        "Italy": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe red.",
+        "Nigeria": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe green.",
+        "Poland": "Flag with two horizontal stripes. Top stripe white, bottom stripe red.",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red.",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background.",
+        "Ukraine": "Flag with two horizontal stripes. Top stripe blue, bottom stripe yellow.",
+        "US": "Flag with many red and white stripes, with white stars on a blue background in the top-left corner."
+    ]
+    
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var score: Int = 0
@@ -47,6 +61,8 @@ struct ContentView: View {
                         Text(countries[correctAnswer])
                             .font(.largeTitle.weight(.semibold))
                     }
+                    .accessibilityElement()
+                    .accessibilityLabel("Tap the flag of \(countries[correctAnswer])")
                     
                     ForEach(0..<3) { number in
                         Button {
@@ -67,6 +83,7 @@ struct ContentView: View {
                         } label: {
                             FlagImage(imageName: countries[number])
                         }
+                        .accessibilityLabel(labels[countries[number], default: "Unknown Flag"])
                         .rotation3DEffect(
                             Angle(degrees: animationAmount3D[number]),
                             axis: (x: -1.0, y: 1.0, z: 20.0)
